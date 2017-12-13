7920e82dc241171713c08628d72d5c84039d957f
JP <jp@jpatrickpark.com>
2017-12-12 23:40:33 -0500

    clean up and rename

--- detection.py
+++ detection.py
@@ -1,4 +1,3 @@
-#import tensorflow as tf
 import numpy as np
 from PIL import Image
 import os
@@ -10,8 +9,7 @@ import util
 import pickle
 
 
-def plot_reconstruction(truth, reconstructed, shape, indices, to_threshold,offsets, box_sizes,num_shown=10,threshold=0.5,filename="default.png"):
-    """Plots reconstructed images below the ground truth images."""
+def plot_detection(truth, reconstructed, shape, indices, to_threshold,offsets, box_sizes,num_shown=10,threshold=0.5,filename="default.png"):
     plt.close()
     for i, image in enumerate(truth[:num_shown]):
         plt.subplot(2, num_shown, i + 1)
@@ -23,35 +21,33 @@ def plot_reconstruction(truth, reconstructed, shape, indices, to_threshold,offse
         plt.axis('off')
         if (len(shape)>2):
             image = (image-np.min(image))/(np.max(image)-np.min(image))
-            #image = np.clip(image,0,1)
         plt.imshow(image.reshape(shape), cmap=plt.cm.gray)
         j = 0
         for y,x in indices[i]:
             if to_threshold[i][j] > threshold:
                 temp.add_patch(patches.Rectangle((x-7+offsets[i][0],y-7+offsets[i][1]),box_sizes[i][0],box_sizes[i][1],linewidth=1,edgecolor='r',facecolor='none'))
-                '''
-                if i==5:
-                    # this deconv layer has offset. I could have implemented it, but it is a quick fix.
-                    temp.add_patch(patches.Rectangle((x,y-4),12,8,linewidth=1,edgecolor='r',facecolor='none'))
-                else:
-                    temp.add_patch(patches.Rectangle((x-7,y-7),12,8,linewidth=1,edgecolor='r',facecolor='none'))
-                '''
             j+=1
     plt.savefig(filename,dpi=300)
     #plt.show()
 
 
-encoded = pickle.load(open("faces_full.p","rb"))
-train, labels = util.read_data_and_labels(3, 64)
 #encoded[0],..., encoded[9] = first 10 images
+encoded = pickle.load(open("faces.p","rb"))
+train, labels = util.read_data_and_labels(3, 64)
+# Selected deconv filters to use
+deconv_filter_indices=[5,33,57,67,72,91,95,127]
+# offsets of bounding boxes which depends on what each convolutional filter looks like
 offsets=[(0,0),(0,-3),(0,0),(0,0),(0,0),(0,0),(0,0),(0,0)]
+# box sizes, same goes with offsets
 box_sizes=[(15,7),(15,9),(14,9),(11,10),(11,7),(12,8),(15,8),(15,10)]
-threshold=0.8
-#mkdir
-for t in range(400):
+# Threshold, whose value is determined by trying out differnt values and choosing one with best performance.
+threshold=0.5
+dirname = "detection"+str(threshold)
+os.mkdir(dirname)
+for t in range(10):
     a, b = [], []
     search_results, nonzero_entries = [], []
-    for i in [5,33,57,67,72,91,95,127]:
+    for i in deconv_filter_indices:
         sparse = encoded[t][0,:,:,i]
         search_result = []
         values = []
@@ -67,7 +63,4 @@ for t in range(400):
         b.append(image)
         search_results.append(search_result)
         nonzero_entries.append(values)
-    plot_reconstruction(a,b,(64,64),search_results,nonzero_entries,offsets,box_sizes,len(offsets),threshold,filename="detection"+str(threshold)+"/"+str(t)+".png")
-
-
-
+    plot_detection(a,b,(64,64),search_results,nonzero_entries,offsets,box_sizes,len(offsets),threshold,filename=dirname+"/"+str(t)+".png")
